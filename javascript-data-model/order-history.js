e99212ddce455331266a23a0c7484afa4b3aa6f7
sal garcia <salgarcia940@gmail.com>
2021-10-22 18:21:33 -0700

    completed javascript data model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,36 @@
+var orderHistory = [
+  {
+    item: 'book',
+    title: 'Javscript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    price: 31.55,
+    orderNumber: 11439416898772232
+  },
+  {
+    item: 'book',
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    price: 41.33,
+    orderNumber: 1139984268128057
+  },
+  {
+    item: 'gamecube controller adapter',
+    price: 15.98,
+    orderNumber: 1142875557905940
+  },
+  {
+    items: [
+      {
+        item: 'GameCube Controller',
+        price: 94.95
+      },
+      {
+        item: 'book',
+        title: 'Faroult, Stephane',
+        price: 33.99
+      }
+    ],
+    orderNumber: 1132283117264824
+  }
+
+];
