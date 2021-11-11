a8c481bef4408c0882e1c5fd6c928570eed34964
sal garcia <salgarcia940@gmail.com>
2021-11-10 21:26:34 -0800

    array manipulation completed

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -2,3 +2,7 @@
 function drop(array, count) {
   return array.slice(count, array.length);
 }
+
+// define the function
+// return the array sliced from count to the total length of
+// the array
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -8,3 +8,13 @@ function includes(array, count) {
   }
   return bool;
 }
+
+// define function
+// define a storage variable bool with the value of false
+// define a for loop with the initialization assigned to 0
+// and the condition checking if 0 is less than the length of the
+// array increment
+// in the for loop code block define an if statement
+// checking if array[i] is equal to the parameter "count"
+// assign true to the bool
+// after that just return the storage variable
