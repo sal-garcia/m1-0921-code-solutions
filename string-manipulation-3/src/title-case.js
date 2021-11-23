ffbe401f601a04f804e492d5d2157da04563f8bb
sal garcia <salgarcia940@gmail.com>
2021-11-22 18:27:42 -0800

    in progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,7 +1,26 @@
 /* exported titleCase */
-/* function titleCase(title) {
-  var word = title.split(' ');
-  var storage = [];
-  for (var i = 0; i < word.length; i++) {
 
-} */
+/*
+
+function titleCase(title) {
+  var word = title.split(' ');
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === 'if') {
+      word[i] = word[i].toLowerCase();
+    } if (word[i] === 'in') {
+      word[i] = word[i].toLowerCase();
+    } if (word[i] === 'of') {
+      word[i] = word[i].toLowerCase();
+    } if (word[i] === 'the') {
+      word[i] = word[i].toLowerCase();
+    } if (word[i] === 'for') {
+      word[i] = word[i].toLowerCase();
+    }
+    console.log(word[i])
+    else {
+      word[i][0].toUpperCase() + word[i].slice(1, word[i].length).toLowerCase();
+    }
+  }
+}
+
+*/
