8a8309d4f20bde13b7e73a9b5b4b1ee70b8fc853
sal garcia <salgarcia940@gmail.com>
2021-11-28 21:40:39 -0800

    string manipulation 3 completed

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -16,3 +16,15 @@ function isAnagram(firstString, secondString) {
     return false;
   }
 }
+// define the function
+// get rid of spaces in the first string parameter
+// do the same for the secondString parameter
+// assign the length of the firststring with no spaces to a variable
+// do the same with the secondString parameter
+// define an if statement checking if the length of the firstString
+// is not equal to that of the secondString, if its not return false
+// split the first string by character and then sort it by alpbaetical order
+// and finally joins it with no spaces inbetween to make the array into a string
+// it does the same with the secondString parameter
+// defines another if condition checking if stringOne is equal to stringTwo
+// if it is it returns true else if returns false
