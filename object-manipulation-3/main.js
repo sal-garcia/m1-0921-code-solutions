01260bca7ab9d04d34030318256a327370746645
sal garcia <salgarcia930@gmail.com>
2021-12-06 18:17:55 -0800

    in progress object manipulation 3

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,21 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// collection of four players
+/* var players = [
+  {
+    name: 'paul',
+    hand: 'right'
+  },
+  {
+    name: 'paul',
+    hand: 'right'
+  },
+  {
+    name: 'paul',
+    hand: 'right'
+  },
+  {
+    name: 'paul',
+    hand: 'right'
+  }
+];
+*/
